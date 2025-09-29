d37b83269970788c83b2d6a1d97f2e7aa8f632b5
RodrigoA15 <rapapamija@misena.edu.co>
2025-09-29 17:36:59 -0500

    Add: Categories module

---
+++ src/categories/categories.service.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Category } from './entities/category.entity';
+import { Model } from 'mongoose';
+import { isEmpty } from 'src/utils/validation.utils';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectModel(Category.name) private categoryModel: Model<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    try {
+      const data = new this.categoryModel(createCategoryDto);
+      return await data.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  async findAll() {
+    const data = await this.categoryModel.find().exec();
+    if (isEmpty(data)) {
+      throw new NotFoundException('Not accidents found');
+    }
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} category`;
+  }
+
+  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    return `This action updates a #${id} category`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} category`;
+  }
+}
