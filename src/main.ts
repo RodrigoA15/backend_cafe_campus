52b4f403db590f76e8cc54d0c5040efa953cf043
RodrigoA15 <rapapamija@misena.edu.co>
2025-10-19 20:46:13 -0500

    Add: Packages and config cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'htpp://localhost:3001',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
